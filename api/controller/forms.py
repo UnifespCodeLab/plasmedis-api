1b171191ff81d97309c188956c1e2450f273da7b
maegima <andre.maegima@unifesp.br>
2022-01-24 13:42:03 -0300

    Separeted post and get functions

--- api/controller/forms.py
+++ api/controller/forms.py
@@ -1,6 +1,6 @@
 from flask_cors import cross_origin
 from api.util.decorators import required
-from api.service.forms import FormSocio, FormSocioGetByUser
+from api.service.forms import PostFormSocio, GetFormSocio, PostFormSocioGetByUser, GetFormSocioGetByUser
 from api import api
 from flask_restx import Resource
 import api.model.request.posts as request
@@ -9,27 +9,30 @@ import api.model.response.default as default
 
 forms = api.namespace('forms', description="Forms namespace")
 
-@forms.route("/<int:id>")
+@forms.route("/")
 class Forms(Resource):
     @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
     @required(response=default.message, token=False)
-    def get(self, id):
-        return FormSocio(id)
-    
+    def post(self, data):
+        return PostFormSocio(data)
+
+
+@forms.route("/<int:id>")
+class FormsId(Resource):
     @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
     @required(response=default.message, token=False)
-    def post(self, id):
-        return FormSocio(id)
-
+    def get(self, id):
+        return GetFormSocio(id)
+    
 @forms.route("/user/<int:id>")
 class FormsUser(Resource):
     @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
     @required(response=default.message, token=False)
     def get(self, id):
-        return FormSocioGetByUser(id)
+        return GetFormSocioGetByUser(id)
 
     @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
     @required(response=default.message, token=False)
-    def post(self, id):
-        return FormSocioGetByUser(id)
+    def post(self, data, id):
+        return PostFormSocioGetByUser(data, id)
 
--- api/service/forms.py
+++ api/service/forms.py
@@ -1,87 +1,69 @@
-from flask import request
 from api import db
 from api.model.database.forms import Form_Socioeconomico
 
-#TODO: separar POST e GET
-#TODO: remover verificação de método
-#TODO: remover verificação de json POST
-#TODO: padronizar respostas dos endpoints?
-def FormSocio(id):
-    if request.method == 'POST':
-        if request.is_json:
-            data = request.get_json()
-            new_form = Form_Socioeconomico(nome_rep_familia=data['nome_rep_familia'], pessoa=data['pessoa'], qtd_pessoas_familia=data['qtd_pessoas_familia'],
-            pessoa_amamenta=data['pessoa_amamenta'], qtd_criancas=data['qtd_criancas'], gestante=data['gestante'], qtd_amamentando=data['qtd_amamentando'], qtd_criancas_deficiencia=data['qtd_criancas_deficiencia'], qtd_gestantes=data['qtd_gestantes'])
-            db.session.add(new_form)
-            db.session.commit()
+def PostFormSocio(data):
+    new_form = Form_Socioeconomico(nome_rep_familia=data['nome_rep_familia'], pessoa=data['pessoa'], qtd_pessoas_familia=data['qtd_pessoas_familia'],
+    pessoa_amamenta=data['pessoa_amamenta'], qtd_criancas=data['qtd_criancas'], gestante=data['gestante'], qtd_amamentando=data['qtd_amamentando'], qtd_criancas_deficiencia=data['qtd_criancas_deficiencia'], qtd_gestantes=data['qtd_gestantes'])
+    db.session.add(new_form)
+    db.session.commit()
 
-            return {"message": f"Formulário enviado!"}
-        else:
-            return {"error": "O envio não foi feita no formato esperado"}
- 
-    elif request.method == 'GET':
-        forms = Form_Socioeconomico.query.filter_by(pessoa=id).all()
-        results = []
-        for form in forms:
-            if form.preenchido:
-                results.append({
-                    "preenchido": form.preenchido,
-                    "nome_rep": form.nome_rep_familia,
-                    "qtd_pessoas": form.qtd_pessoas_familia,
-                    "qtd_criancas": form.qtd_criancas,
-                    "gestante": form.gestante,
-                    "qtd_amamentando": form.qtd_amamentando,
-                    "qtd_criancas_deficiencia": form.qtd_criancas_deficiencia,
-                    "pessoa_amamenta": form.pessoa_amamenta,
-                    "qtd_gestantes": form.qtd_gestantes
-                })
+    return {"message": f"Formulário enviado!"}
 
-        return {"count": len(results), "users": results}
-
-#TODO: separar POST e GET
-#TODO: remover verificação de método
-#TODO: remover verificação de json POST
-def FormSocioGetByUser(user_id):
-    if request.method == 'POST':
-        if request.is_json:
-            data = request.get_json()
-            form = Form_Socioeconomico.query.filter_by(pessoa=user_id).first()
-            if form is None:
-                new_form = Form_Socioeconomico(nome_rep_familia=data['nome_rep_familia'], pessoa=user_id, qtd_pessoas_familia=data['qtd_pessoas_familia'],
-                pessoa_amamenta=data['pessoa_amamenta'], qtd_criancas=data['qtd_criancas'], gestante=data['gestante'], qtd_amamentando=data['qtd_amamentando'], qtd_criancas_deficiencia=data['qtd_criancas_deficiencia'], qtd_gestantes=data['qtd_gestantes'])
-                db.session.add(new_form)
-                db.session.commit()
-                return {"status":"1000", "message":"created"}
-
-            else:
-                form.nome_rep_familia = data['nome_rep_familia']
-                form.qtd_pessoas_familia = data['qtd_pessoas_familia']
-                form.pessoa_amamenta = data['pessoa_amamenta']
-                form.qtd_criancas = data['qtd_criancas']
-                form.gestante = data['gestante']
-                form.qtd_amamentando = data['qtd_amamentando']
-                form.qtd_criancas_deficiencia = data['qtd_criancas_deficiencia']
-                form.qtd_gestantes = data['qtd_gestantes']
-
-                db.session.add(form)
-                db.session.commit()
-
-                return {"status":"1000", "message":"updated"}
-        else:
-            return {"error": "O envio não foi feita no formato esperado"}
-    
-    elif request.method == 'GET':
-        form = Form_Socioeconomico.query.filter_by(pessoa=user_id).one()
-        response = {
-                "nome_rep_familia": form.nome_rep_familia,
-                "qtd_pessoas_familia": form.qtd_pessoas_familia,
-                "qtd_criancas": form.qtd_criancas,
-                "pessoa_amamenta": form.pessoa_amamenta,
+def GetFormSocio(id):
+    forms = Form_Socioeconomico.query.filter_by(pessoa=id).all()
+    results = []
+    for form in forms:
+        if form.preenchido:
+            results.append({
+                "preenchido": form.preenchido,
+                "nome_rep": form.nome_rep_familia,
+                "qtd_pessoas": form.qtd_pessoas_familia,
                 "qtd_criancas": form.qtd_criancas,
                 "gestante": form.gestante,
                 "qtd_amamentando": form.qtd_amamentando,
                 "qtd_criancas_deficiencia": form.qtd_criancas_deficiencia,
-                "qtd_gestantes": form.qtd_gestantes,
-        }
+                "pessoa_amamenta": form.pessoa_amamenta,
+                "qtd_gestantes": form.qtd_gestantes
+            })
 
-        return {"message": "success", "form": response}
+    return {"count": len(results), "users": results}
+
+def PostFormSocioGetByUser(data, user_id):
+    form = Form_Socioeconomico.query.filter_by(pessoa=user_id).first()
+    if form is None:
+        new_form = Form_Socioeconomico(nome_rep_familia=data['nome_rep_familia'], pessoa=user_id, qtd_pessoas_familia=data['qtd_pessoas_familia'],
+        pessoa_amamenta=data['pessoa_amamenta'], qtd_criancas=data['qtd_criancas'], gestante=data['gestante'], qtd_amamentando=data['qtd_amamentando'], qtd_criancas_deficiencia=data['qtd_criancas_deficiencia'], qtd_gestantes=data['qtd_gestantes'])
+        db.session.add(new_form)
+        db.session.commit()
+        return {"status":"1000", "message":"created"}
+
+    else:
+        form.nome_rep_familia = data['nome_rep_familia']
+        form.qtd_pessoas_familia = data['qtd_pessoas_familia']
+        form.pessoa_amamenta = data['pessoa_amamenta']
+        form.qtd_criancas = data['qtd_criancas']
+        form.gestante = data['gestante']
+        form.qtd_amamentando = data['qtd_amamentando']
+        form.qtd_criancas_deficiencia = data['qtd_criancas_deficiencia']
+        form.qtd_gestantes = data['qtd_gestantes']
+
+        db.session.add(form)
+        db.session.commit()
+
+        return {"status":"1000", "message":"updated"}
+
+def GetFormSocioGetByUser(user_id):
+    form = Form_Socioeconomico.query.filter_by(pessoa=user_id).one()
+    response = {
+            "nome_rep_familia": form.nome_rep_familia,
+            "qtd_pessoas_familia": form.qtd_pessoas_familia,
+            "qtd_criancas": form.qtd_criancas,
+            "pessoa_amamenta": form.pessoa_amamenta,
+            "qtd_criancas": form.qtd_criancas,
+            "gestante": form.gestante,
+            "qtd_amamentando": form.qtd_amamentando,
+            "qtd_criancas_deficiencia": form.qtd_criancas_deficiencia,
+            "qtd_gestantes": form.qtd_gestantes,
+    }
+
+    return {"message": "success", "form": response}
