1e3ec8267ad4e1a0c4716fee69a697f573f14876
maegima <andre.maegima@unifesp.br>
2022-01-24 13:28:17 -0300

    Added documentation for forms_socio endpoints

--- api/controller/forms.py
+++ api/controller/forms.py
@@ -1,23 +1,35 @@
 from flask_cors import cross_origin
-from api.util.decorators import token_required
+from api.util.decorators import required
 from api.service.forms import FormSocio, FormSocioGetByUser
-from flask import Blueprint
+from api import api
+from flask_restx import Resource
+import api.model.request.posts as request
+import api.model.response.posts as response
+import api.model.response.default as default
 
-#TODO: adicionar prefixo para as chamadas
-app = Blueprint('forms', __name__, url_prefix='')
+forms = api.namespace('forms', description="Forms namespace")
 
-#TODO: separar POST e GET
-#TODO: adicionar json_required POST
-@app.route('/form_socio/<id>', methods=['POST', 'GET'])
-@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
-@token_required
-def form_socio(id):
-    return FormSocio(id)
+@forms.route("/<int:id>")
+class Forms(Resource):
+    @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
+    @required(response=default.message, token=False)
+    def get(self, id):
+        return FormSocio(id)
+    
+    @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
+    @required(response=default.message, token=False)
+    def post(self, id):
+        return FormSocio(id)
 
-#TODO: separar POST e GET
-#TODO: adicionar json_required POST
-@app.route('/form_socio_by_user_id/<user_id>', methods=['POST', 'GET'])
-@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
-@token_required
-def form_socio_get_by_user(user_id):
-    return FormSocioGetByUser(user_id)
+@forms.route("/user/<int:id>")
+class FormsUser(Resource):
+    @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
+    @required(response=default.message, token=False)
+    def get(self, id):
+        return FormSocioGetByUser(id)
+
+    @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
+    @required(response=default.message, token=False)
+    def post(self, id):
+        return FormSocioGetByUser(id)
+
