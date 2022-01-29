7ccae0e5d9f24a2f6eac47c1bb40954de9b6e32c
maegima <andre.maegima@unifesp.br>
2022-01-29 17:59:50 -0300

    Added token required to comments endpoint

--- api/controller/comments.py
+++ api/controller/comments.py
@@ -12,18 +12,18 @@ comments = api.namespace('comments', description="Comments namespace")
 @comments.route("/")
 class Comments(Resource):
     @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
-    @required(response=default.message, request=request.comment, token=False)
+    @required(response=default.message, request=request.comment, token=True)
     def post(self, data):
         return PostComentarios(data)
     
     @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
-    @required(response=response.comment_list, token=False)
+    @required(response=response.comment_list, token=True)
     def get(self):
         return GetComentarios()
 
 @comments.route("/post/<int:id>")
 class CommentsPost(Resource):
     @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
-    @required(response=response.comment_creator_list, token=False)
+    @required(response=response.comment_creator_list, token=True)
     def get(self, id):
         return GetComentariosPostagem(id)
