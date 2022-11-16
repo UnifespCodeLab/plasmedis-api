c1d14efd6f0f2fef5aff3e31eed789b5a00c5789
gacneves <gabriel.angelo1008@gmail.com>
2022-11-16 18:19:59 -0300

    removido schemas padroes desnecessarios

--- api/model/response/default.py
+++ api/model/response/default.py
@@ -4,13 +4,3 @@ from flask_restx import fields
 message = api.model("Message", {
     "message": fields.String()
 })
-
-success_message = api.model("Success Message", {
-    "success": fields.Boolean(),
-    "message": fields.String()
-})
-
-status_message = api.model("Status Message", {
-    "status": fields.String(),
-    "message": fields.String()
-})
